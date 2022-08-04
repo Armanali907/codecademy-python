419a754e823438437acdb97993221ee8e692449a
Arman Ali Ansari <68926141+Armanali907@users.noreply.github.com>
2022-08-04 13:48:51 +0530

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,10 @@
+# Arman Ali
+# Wanna be Web Developer
+
+print("  A       A  ")
+print(" A A     A A ")
+print("A   A   A   A")
+print("AAAAA   AAAAA")
+print("A   A   A   A")
+print("A   A   A   A")
+print("A   A   A   A")
